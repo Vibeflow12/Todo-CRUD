4116689daa0b2dab240f8fe031b9359102500828
Prashanth <kasamsaimaniprashanth@gmail.com>
2024-04-23 13:31:48 +0530

    client

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export default function App() { 
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    async function getTodos () {
+      const res = await fetch ("api/todos");
+      const todos = await res.json();
+      setTodos(todos);
+    }
+    getTodos();
+  },[]);
+
+  return (
+    <main className="container">
+      <h1 className="title">Awesome Todos</h1>
+      {(todos.length > 0 ) &&
+        todos.map((todo) => {
+          return (
+            <div key={todo._id} className="todo">
+              <p>{todo.todo}</p>
+              <div>
+                <button>
+                  {(todo.status) ? "☑" : "☐"}
+                </button>
+              </div>
+            </div>
+          );
+        })}
+    </main>
+  );
+      } 
